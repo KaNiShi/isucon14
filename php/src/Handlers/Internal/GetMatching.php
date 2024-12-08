0c97f5f20beca290c0fae2316b9f872efc59dd55
KaNiShi <kanishi.kuu@gmail.com>
2024-12-08 16:30:54 +0900

    スコア調整

--- php/src/Handlers/Internal/GetMatching.php
+++ php/src/Handlers/Internal/GetMatching.php
@@ -72,7 +72,7 @@ ORDER BY distance
             $scores = [];
             foreach ($candidates as $candidate) {
                 $score = $distance / $candidate['speed'] + $candidate['distance'] / $candidate['speed'];
-                if ($score < 7) {
+                if ($score < 100) {
                     continue;
                 }
 
