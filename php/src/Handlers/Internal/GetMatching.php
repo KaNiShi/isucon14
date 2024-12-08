721e03359f9cd6f9ad9b8eaf0bf0e0e85fe38acf
KaNiShi <kanishi.kuu@gmail.com>
2024-12-08 11:43:03 +0900

    10人マッチにしてみる

--- php/src/Handlers/Internal/GetMatching.php
+++ php/src/Handlers/Internal/GetMatching.php
@@ -22,16 +22,16 @@ class GetMatching extends AbstractHttpHandler
         ResponseInterface $response,
     ): ResponseInterface {
         // MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
-        $stmt = $this->db->prepare('SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1');
+        $stmt = $this->db->prepare('SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 10');
         $stmt->execute();
-        $ride = $stmt->fetch(PDO::FETCH_ASSOC);
+        $rides = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        if (!$ride) {
+        if (!$rides) {
             return $this->writeNoContent($response);
         }
 
         $stmt = $this->db->prepare(
-            'SELECT * FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 10'
+            'SELECT * FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 100'
         );
         $stmt->execute();
         $matched = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -39,17 +39,20 @@ class GetMatching extends AbstractHttpHandler
             return $this->writeNoContent($response);
         }
 
-        foreach ($matched as $item) {
-            $stmt = $this->db->prepare(
-                'SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE'
-            );
-            $stmt->execute([$item['id']]);
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            $empty = $result['COUNT(*) = 0'];
-            if ($empty) {
-                $stmt = $this->db->prepare('UPDATE rides SET chair_id = ? WHERE id = ?');
-                $stmt->execute([$item['id'], $ride['id']]);
-                return $this->writeNoContent($response);
+        foreach ($rides as $ride) {
+            foreach ($matched as $key => $item) {
+                $stmt = $this->db->prepare(
+                    'SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE'
+                );
+                $stmt->execute([$item['id']]);
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                $empty = $result['COUNT(*) = 0'];
+                unset($matched[$key]);
+                if ($empty) {
+                    $stmt = $this->db->prepare('UPDATE rides SET chair_id = ? WHERE id = ?');
+                    $stmt->execute([$item['id'], $ride['id']]);
+                    break;
+                }
             }
         }
 
